a35ddfb349d0cd3dc9926960181b04fb75b73111
Mike Heron <mikeheron@Mikes-MBP.lan>
2025-02-09 17:14:56 -0500

    Added package.json and index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to BeatHustle Business Affairs Chatbot</h1>
+      <p>Ask me anything about music industry contracts!</p>
+    </div>
+  );
+}
+
