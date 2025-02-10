1dc1809476f4844e3a7d0166e086ced352be9187
Mike Heron <mikeheron@Mikes-MBP.lan>
2025-02-09 20:54:36 -0500

    Added pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -7,3 +7,12 @@ export default function Home() {
   );
 }
 
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to BeatHustle Business Affairs Chatbot</h1>
+      <p>Ask me anything about music industry contracts!</p>
+    </div>
+  );
+}
+
